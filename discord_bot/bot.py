150024d5305e0f5c3cac3bbc378668f5ec0777ac
ling-nyc <lingpeng1209@outlook.com>
2022-01-15 11:42:34 -0500

    I added some stuff from a bot template, so now we should have somehting to work off of

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,21 +1,53 @@
+# discord api
 import discord
-import dotenv
-import os
 from discord.ext import commands
 
+# .env configuration
+from dotenv import load_dotenv
+load_dotenv()
 
-#sheeesh
-dotenv.load_dotenv()
-intents = discord.Intents.all()
-client = commands.Bot(command_prefix="!", intents=intents)
+# system
+import psutil
+import os
 
-@client.event
-async def on_ready():
-    print("bot is ready")
+# custom utilities and setup
+from Utilities import log
 
-@client.command()
-async def test(ctx):
-    await ctx.send("jordan is a dumbass")
+log = log.Logger("client")
 
+# client runtime variable
+Token = os.getenv("CLIENT_TOKEN")
 
-client.run(os.environ.get('token'))
+class Client(commands.AutoShardedBot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # client version
+        self.Version = "0.1.0"
+
+        # operational level variables
+        self.Updating = False
+        self.Debugging = False
+        self.Maintaining = False
+
+        # psutil utilization
+        self.process = psutil.Process(os.getpid())
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await log.info(f"{self.user} is online.")
+
+# calling and initializing the client
+client = Client(
+    commands.when_mentioned_or("."),
+    intents=discord.Intents.all(),
+    case_insensitive=True,
+    help_command=None
+)
+
+# Load cogs
+for filename in os.listdir("./Cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"Cogs.{filename[:-3]}")
+
+client.run(Token, reconnect=True)
